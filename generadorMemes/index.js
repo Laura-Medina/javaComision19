2892421e050201c37addf09f46aa7e46e7259c77
Laura <lauramedinamarisol@gmail.com>
2023-10-16 22:36:03 +0200

    modo oscuro y asides

--- generadorMemes/index.js
+++ generadorMemes/index.js
@@ -1,11 +1,30 @@
-const toggleButton = document.getElementById("theme-toggle");
-const body = document.body;
 
-toggleButton.addEventListener("click", () => {
-    if (body.classList.contains("light-mode")) {
-        body.classList.remove("light-mode");
-      } else {
-        body.classList.add("light-mode");
-      }
-    });
+const $ = (element) => document.querySelector(element);
+const darkThemeButton = $('#theme-toggle');
 
+
+darkThemeButton.addEventListener('click', () => {
+	document.body.classList.toggle('light-mode');
+	document.body.classList.toggle('dark-theme');
+    if ($('#theme-toggle').innerHTML === 'Dark Mode') {
+		$('#theme-toggle').innerHTML = 'Light Mode';
+	} else {
+		$('#theme-toggle').innerHTML = 'Dark Mode';
+	}
+});
+
+
+const buttonText = $('#button-text');
+const buttonImg = $('#button-img');
+const asideBar = $('#asideImg');
+const asideTexto = $('#asideTexto');
+
+buttonText.addEventListener('click', () => {
+	asideBar.style.display = 'none';
+	asideTexto.style.display = 'block';
+});
+
+buttonImg.addEventListener('click', () => {
+	asideTexto.style.display = 'none';
+	asideBar.style.display = 'block';
+});
